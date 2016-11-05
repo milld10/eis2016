08362cae02240f34bfb49e13f4342195a6795870
Thomas Hödl <thomas.hoedl@student.tugraz.at>
2016-11-05 11:32:38 +0100

    first try collision detection finished

--- Task2_TH/pycharm_project_base/collision_search.py
+++ Task2_TH/pycharm_project_base/collision_search.py
@@ -1,34 +1,15 @@
-# Your task is to find a collision for a modified version of the SHA-2 hash function.
-# We use the SHA-256, which generates a 256-bit output.
-# For the exercises, the output of the hash function is truncated to 64 bits using the following
-# rule: let h = A∥B∥C∥D∥E∥F∥G∥H the 256-bit output of SHA-256, with A, B, C, D, E, F, G and H being 32-bit chunks.
-# The modified hash is computed using a bitwise XOR and aconcatenation: h′ =E⊕F⊕G⊕H∥A⊕B⊕C⊕D.
-
-# Your colliding messages need to start with the same prefix, which is a concatenation of your matriculation
-# numbers in ascending order (in standard ASCII encoding). There are no restrictions for the message after this prefix.
-# For instance, a group with members 1030123 and 1030456 needs to find colliding messages of form m1 = 10301231030456X and
-# m1 = 10301231030456Y , with X ̸= Y and a freely chosen length of X and Y .
-
-# SHA-256("10301231030456") = 0xc899398cd246818ffab4ba8842f3251ac2fc27d690979d1891322345818ff381
-# SHA-256-mod("10301231030456") = 0x42d66a0aa2982791
-# You should use an existing SHA-2 implementation, do not implement it yourself.
-# The memory requirements of your program must not exceed 2GB (RAM and HDD).
-# Look for solutions that do not require such a large amount of memory.
-
-# Tip:
-# Start off with collision search for smaller hashes, e.g., only the first 32 bits of the hash.
-# Expect that the collision search takes some time. Depending on your code and machine, the search can take more than an hour.
-
-
-
 import hashlib
+import time
 
 CONST_PREFIX = '1430320143050014305291430751'
+CONST_N = 1<<1000000
+CONST_START_NUMBER = '2ac5'
+CONST_LIST_SIZE = 100
 
-def SHA2mod (prefix, message):
+def SHA2mod(prefix, message):
     colliding_message = prefix + message
     hash_object = hashlib.sha256(colliding_message.encode('utf-8'))
-    print(hash_object.hexdigest())
+    #print(hash_object.hexdigest())
     chunkA = hash_object.hexdigest()[:8]
     chunkB = hash_object.hexdigest()[8:16]
     chunkC = hash_object.hexdigest()[16:24]
@@ -39,15 +20,54 @@ def SHA2mod (prefix, message):
     chunkH = hash_object.hexdigest()[56:64]
     #print(chunkA, chunkB, chunkC, chunkD, chunkE, chunkF, chunkG, chunkH)
 
-    XOR_ABCD_2 = hex(int(chunkA, 16) ^ int(chunkB, 16) ^ int(chunkC, 16) ^ int(chunkD, 16))
-    XOR_EFGH_1 = hex(int(chunkE, 16) ^ int(chunkF, 16) ^ int(chunkG, 16) ^ int(chunkH, 16))
-    XOR = XOR_EFGH_1+XOR_ABCD_2
-    #print(XOR)
+    XOR_ABCD_2 = int(chunkA, 16) ^ int(chunkB, 16) ^ int(chunkC, 16) ^ int(chunkD, 16)
+    XOR_EFGH_1 = int(chunkE, 16) ^ int(chunkF, 16) ^ int(chunkG, 16) ^ int(chunkH, 16)
+    XOR = (XOR_EFGH_1<<32)|XOR_ABCD_2
+    #print("%x" % XOR_ABCD_2)
+    #print("%x" % XOR_EFGH_1)
+    #print("%x" % XOR)
+    return "%x" % XOR
+    #return "%x" % XOR_ABCD_2
 
-def generateMessage ():
+def searchCollision():
+    tortoiseVal = SHA2mod(CONST_PREFIX, CONST_START_NUMBER)
+    hareVal = SHA2mod(CONST_PREFIX, SHA2mod(CONST_PREFIX, CONST_START_NUMBER))
+    oldTortoiseVal = ''
+    oldHareVal = ''
 
+    while(hareVal != tortoiseVal):
+        oldTortoiseVal = tortoiseVal
+        oldHareVal = hareVal
+        tortoiseVal = SHA2mod(CONST_PREFIX, tortoiseVal)
+        hareVal = SHA2mod(CONST_PREFIX, SHA2mod(CONST_PREFIX, hareVal))
 
+    print("collision detected: now find where")
 
+    hareValBegin = hareVal
+    hareValues = dict()
+    oldHareVal = hareValBegin
+    hareVal = SHA2mod(CONST_PREFIX, hareValBegin)
+    while (hareVal != hareValBegin):
+        hareValues[hareVal] = oldHareVal
+        oldHareVal = hareVal
+        hareVal = SHA2mod(CONST_PREFIX, hareVal)
+    hareValues[hareVal] = oldHareVal
 
-SHA2mod(CONST_PREFIX, random_message)
+    tortoiseVal = CONST_START_NUMBER
 
+    while (tortoiseVal not in hareValues):
+        oldTortoiseVal = tortoiseVal
+        tortoiseVal = SHA2mod(CONST_PREFIX, tortoiseVal)
+
+    oldHareVal = hareValues[tortoiseVal]
+
+    print('Collision was detected!!!')
+    print('Collision with hash value ' + tortoiseVal)
+    print('Normal value tortoise: ' + CONST_PREFIX + oldTortoiseVal)
+    print('Normal value hare:     ' + CONST_PREFIX + oldHareVal)
+    print('Control tortoise: ' + SHA2mod(CONST_PREFIX, oldTortoiseVal))
+    print('Control hare:     ' + SHA2mod(CONST_PREFIX, oldHareVal))
+
+searchCollision()
+#print('Control tortoise: ' + SHA2mod(CONST_PREFIX, 'f14e6ba5'))
+#print('Control hare:     ' + SHA2mod(CONST_PREFIX, '5d1b5e27'))
